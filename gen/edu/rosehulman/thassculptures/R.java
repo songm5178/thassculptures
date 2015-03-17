571f64d0b056295d5e4f24f06211641f83d98d0a
songm <songm@rose-hulman.edu>
2015-03-17 12:47:13 -0400

    added main layout and sculptures

--- gen/edu/rosehulman/thassculptures/R.java
+++ gen/edu/rosehulman/thassculptures/R.java
@@ -22,21 +22,54 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int art_spaces_logo=0x7f020000;
+        public static final int gatekeepercoll=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080010;
+        public static final int art_spaces_imageview=0x7f080000;
+        public static final int button_donate=0x7f080008;
+        public static final int button_gallery=0x7f080003;
+        public static final int button_map=0x7f080002;
+        public static final int button_tours=0x7f080005;
+        public static final int button_website=0x7f080006;
+        public static final int imageView_sculpture=0x7f080009;
+        public static final int layout_donate=0x7f080007;
+        public static final int layout_map_gallery=0x7f080001;
+        public static final int layout_tours_website=0x7f080004;
+        public static final int listView1=0x7f08000f;
+        public static final int map_temp=0x7f08000e;
+        public static final int textView_sculptureAddress=0x7f08000c;
+        public static final int textView_sculptureAuthor=0x7f08000b;
+        public static final int textView_sculptureDescription=0x7f08000d;
+        public static final int textView_sculptureName=0x7f08000a;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int activity_sculpture=0x7f030001;
+        public static final int activity_tours_map=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
+        public static final int sculpture=0x7f070001;
+        public static final int tours_map=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050002;
         public static final int app_name=0x7f050000;
+        public static final int donate=0x7f050007;
+        public static final int gallery=0x7f050004;
         public static final int hello_world=0x7f050001;
+        public static final int map=0x7f050003;
+        public static final int sculpture_address=0x7f05000c;
+        public static final int sculpture_author=0x7f05000b;
+        public static final int sculpture_description=0x7f05000d;
+        public static final int sculpture_name=0x7f05000a;
+        public static final int title_activity_sculpture=0x7f050009;
+        public static final int title_activity_tours_map=0x7f050008;
+        public static final int tours=0x7f050005;
+        public static final int website=0x7f050006;
     }
     public static final class style {
         /** 
--- src/edu/rosehulman/thassculptures/MainActivity.java
+++ src/edu/rosehulman/thassculptures/MainActivity.java
@@ -1,16 +1,57 @@
 package edu.rosehulman.thassculptures;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+
+	protected static final int REQUEST_CODE_CHANGE_BUTTON = -1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button toursButton = (Button) findViewById(R.id.button_tours);
+		toursButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), ToursMapActivity.class);
+				startActivityForResult(intent, REQUEST_CODE_CHANGE_BUTTON);
+
+			}
+		});
+		Button mapButton = (Button) findViewById(R.id.button_map);
+		mapButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), ToursMapActivity.class);
+				startActivityForResult(intent, REQUEST_CODE_CHANGE_BUTTON);
+
+			}
+		});
+
+		Button galleryButton = (Button) findViewById(R.id.button_gallery);
+		galleryButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent(getApplicationContext(), SculptureActivity.class);
+				startActivityForResult(intent, REQUEST_CODE_CHANGE_BUTTON);
+
+			}
+		});
 	}
 
 	@Override
