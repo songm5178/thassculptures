b63efedf9b0a81f4a9dc1b4036eecd54e9e1aa5b
songm <songm@rose-hulman.edu>
2015-04-14 12:51:23 -0400

    generating api

--- src/edu/rosehulman/thassculptures/ToursMapActivity.java
+++ src/edu/rosehulman/thassculptures/ToursMapActivity.java
@@ -2,29 +2,43 @@ package edu.rosehulman.thassculptures;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class ToursMapActivity extends Activity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class ToursMapActivity extends FragmentActivity implements OnMapReadyCallback {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_tours_map);
+		MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+		mapFragment.getMapAsync(this);
 		ListView listView = (ListView) findViewById(R.id.listview_sculpture);
-		
+
 		ArrayList<String> names = new ArrayList<String>();
 		names.add("James");
 		names.add("Min");
 		names.add("Rocco");
 		names.add("Munchi");
-		
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, names);
 		listView.setAdapter(adapter);
+	}
+
+	@Override
+	public void onMapReady(GoogleMap map) {
+		map.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
 	}
 
 	@Override
@@ -45,4 +59,5 @@ public class ToursMapActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+
 }
